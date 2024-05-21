3c519a1f7f204c9015dc8d24b33627f5bfdfeb56
giorgiacek <giorgivola@gmail.com>
2024-05-21 17:15:47 -0400

    new draft of dist_stats steps

--- dist_stats_steps_v2.R
+++ dist_stats_steps_v2.R
@@ -1,14 +1,20 @@
 # 1. Arguments ----
-cache_tb <- pipload::pip_load_cache("NGA", version = '20240326_2017_01_02_PROD') # micro, group, imputed
-cache_CHN <- pipload::pip_load_cache("CHN", version = '20240326_2017_01_02_PROD') # aggregate
+tic()
+# NGA for general cases, CHN for aggregate, BOL for levels in group data
+cache_tb <- pipload::pip_load_cache(c("NGA", "CHN", "BOL"), 
+                                    version = '20240326_2017_01_02_PROD')
+toc() # takes a bit: ~2 minutes
+
+
 mean_table <- svy_mean_ppp_table_sac
-# pop_table <- dl_aux$pop reporting_pop in mean_table
+# pop_table <- dl_aux$pop reporting_pop in mean_table right?
 ppp_year <- py
 
 
-# 2. Check which distribution types and which areas (levels) in the set
+# 2. Check which distribution types/areas (levels)/grep in the set
 distributions <- funique(cache_tb$distribution_type)
 areas <- funique(cache_tb$area)
+d2 <- grepl("D2", funique(cache_tb$cache_id))
 
 # 3. Fill area -----
 # Fill area with national when empty and reporting_level for aggregate distribution
@@ -17,31 +23,38 @@ cache_tb <- ftransform(cache_tb, area = ifelse(distribution_type == "aggregate",
                                                ifelse(as.character(area) == "",
                                                       "national", as.character(area))))
 
+cache_tb <- cache_tb |> ftransform(d2 = ifelse(grepl("D2", cache_id), 1, 0))
+
 #cache_CHN <- ftransform(cache_CHN, area = ifelse(distribution_type == "aggregate",
 #                                               as.character(reporting_level),
 #                                               ifelse(as.character(area) == "",
 #                                                      "national", as.character(area))))
 
+
+
+
 if ("micro" %in% distributions){
   # Micro data have national and level data estimations:
-  if ("rural" | "urban" %in% areas) {
+  if ("rural" %in% areas || "urban" %in% areas) {
     # 3. Micro and Imputed Data ----
-    ## 3.1 Micro Level Estimates ----
-    md_dt_area<- 
+    ## 3.1 Level Estimates ----
+    md_id_dt_area<- 
       cache_tb |>
       fselect(cache_id, country_code, surveyid_year, distribution_type,
-              reporting_level, area, welfare, weight, welfare_ppp) |>
-      fsubset(distribution_type == 'micro') |>
-      joyn::joyn(mean_table |> fsubset(distribution_type == 'micro'),
-                 by=c("cache_id", "surveyid_year", "reporting_level", "area"), 
+              reporting_level, imputation_id, area, welfare, weight, welfare_ppp) |>
+      fsubset(distribution_type %in% c("imputed", "micro")) |>
+      joyn::joyn(mean_table |> fsubset(distribution_type %in% c("imputed", "micro")),
+                 by=c("cache_id", "surveyid_year", "reporting_level", "area"), # by "area" needs to be added back here 
                  y_vars_to_keep = c("survey_mean_ppp"),
                  match_type = "m:1",
                  reportvar = FALSE,
-                 keep = "left")|>
-      roworder(cache_id, country_code, surveyid_year, distribution_type,
-               reporting_level, area, welfare_ppp) |>
+                 keep = "left") |>
+      # welfare needs to be ordered (but not by area)
+      # imputation_id is only for imputed, but can be left in for micro, as there is only one value (empty)
+      roworder(cache_id, country_code, surveyid_year, distribution_type, 
+               reporting_level, imputation_id, area, welfare_ppp) |>
       fgroup_by(cache_id, country_code, surveyid_year, distribution_type,
-                reporting_level, area)|>
+                reporting_level, imputation_id, area)|> 
       fsummarise(res = list(wbpip:::md_compute_dist_stats(  
         welfare = welfare_ppp,
         weight = weight,
@@ -50,55 +63,23 @@ if ("micro" %in% distributions){
         Statistic = names(unlist(res)), 
         Value = unlist(res))),
         by = .(cache_id, country_code, surveyid_year, distribution_type,
-               reporting_level, area)] |>
+               reporting_level, imputation_id, area)] |>
       dcast(cache_id + country_code + surveyid_year + distribution_type + 
-              reporting_level + area ~ Statistic, 
-            value.var = "Value")
+              reporting_level + imputation_id + area ~ Statistic, 
+            value.var = "Value") |>
+      fgroup_by(cache_id, country_code, surveyid_year, area)|>
+      fsummarise(across(gini:quantiles9, fmean))
   }
   
-  ## 3.2 Micro National Dist Estimates ----
-  md_dt_national<- 
-    cache_tb |>
-    fselect(cache_id, country_code, surveyid_year, distribution_type,
-            reporting_level, area, welfare, weight, welfare_ppp) |>
-    fsubset(distribution_type == 'micro') |>
-    joyn::joyn(mean_table |> fsubset(distribution_type == 'micro' 
-                                     & area == "national"),
-               by=c("cache_id", "surveyid_year", "reporting_level"), 
-               y_vars_to_keep = c("survey_mean_ppp"),
-               match_type = "m:1",
-               reportvar = FALSE,
-               keep = "left") |>
-    # welfare needs to be ordered (but not by area)
-    roworder(cache_id, country_code, surveyid_year, distribution_type, 
-             reporting_level, welfare_ppp) |>
-    fgroup_by(cache_id, country_code, surveyid_year, distribution_type,
-              reporting_level)|> # no area so dist stat are the sum of both
-    fsummarise(res = list(wbpip:::md_compute_dist_stats(  
-      welfare = welfare_ppp,
-      weight = weight,
-      mean = funique(survey_mean_ppp))))|>
-    _[, c(.SD, .( # using _ because we are using native pipe 
-      Statistic = names(unlist(res)), 
-      Value = unlist(res))),
-      by = .(cache_id, country_code, surveyid_year, distribution_type,
-             reporting_level)] |>
-    dcast(cache_id + country_code + surveyid_year + distribution_type + 
-            reporting_level ~ Statistic, 
-          value.var = "Value")
-  
-}
-
-if ("imputed" %in% distributions){
-  
-  id_dt_national<- 
+  ## 3.2 National Estimates ----
+  md_id_dt_national<- 
     cache_tb |>
     fselect(cache_id, country_code, surveyid_year, distribution_type,
             reporting_level, imputation_id, area, welfare, weight, welfare_ppp) |>
     fsubset(distribution_type %in% c("imputed", "micro")) |>
     joyn::joyn(mean_table |> fsubset(distribution_type %in% c("imputed", "micro") 
                                      & area == "national"),
-               by=c("cache_id", "surveyid_year", "reporting_level"), # by "area" needs to be added back here 
+               by=c("cache_id", "surveyid_year", "reporting_level"),
                y_vars_to_keep = c("survey_mean_ppp"),
                match_type = "m:1",
                reportvar = FALSE,
@@ -122,8 +103,9 @@ if ("imputed" %in% distributions){
           value.var = "Value") |>
     fgroup_by(cache_id, country_code, surveyid_year)|>
     fsummarise(across(gini:quantiles9, fmean))
-    
+  
 }
+
 
 if ("group" %in% distributions){
   

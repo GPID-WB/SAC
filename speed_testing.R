031854b536a60e7d4d41f98af5427ab5eb584571
Diana Garcia <dgarciarojas@worldbank.org>
2024-06-04 09:37:35 -0400

    Speed test for the wrapper of md_compute_dist_stats from wbpip

--- speed_testing.R
+++ speed_testing.R
@@ -57,5 +57,34 @@ if (requireNamespace("highcharter")) {
   
 } else {
   boxplot(bench, outline = FALSE)
+} 
+
+bench_dist <- microbenchmark::microbenchmark(
+  times = 100,
+  wbpip = {
+    new_value = wbpip:::md_compute_dist_stats(dt$welfare_ppp,
+                                              dt$weight)
+  },
+  wbpip_wpr = {
+    old_value = wrp_md_dist_stats(dt$welfare_ppp,
+                                  dt$weight)
+  })
+
+
+if (requireNamespace("highcharter")) {
+  hc_dt <- highcharter::data_to_boxplot(bench_dist,
+                                        time,
+                                        expr,
+                                        add_outliers = FALSE,
+                                        name = "Time in milliseconds")
+  
+  highcharter::highchart() |>
+    highcharter::hc_xAxis(type = "category") |>
+    highcharter::hc_chart(inverted=TRUE) |>
+    highcharter::hc_add_series_list(hc_dt) |>
+    highcharter::hc_title(text = "Comparison wbpip and wbpip wrapper (Dist Stats)")
+  
+} else {
+  boxplot(bench, outline = FALSE)
 }
 

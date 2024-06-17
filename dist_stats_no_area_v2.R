c0b33705bdabda676f2ddf8ebee0a9de656c5546
giorgiacek <giorgivola@gmail.com>
2024-06-17 14:05:59 -0400

    final steps with full table

--- dist_stats_no_area_v2.R
+++ dist_stats_no_area_v2.R
@@ -1,6 +1,6 @@
 # cache_sac used instead of cache, make sure you have cache_sac created first
 # This file creates "final" which is comparable to the TAR version.
-# mean_table <- means_out_sac
+mean_table <- means_out_sac
 
 # 1. Select variables and subset for Micro data----
 
@@ -8,7 +8,7 @@ dt_m <- cache_sac |>
   fselect(cache_id, distribution_type, cpi_data_level, ppp_data_level,
           gdp_data_level, pce_data_level,
           pop_data_level, reporting_level, 
-          imputation_id, area, weight, welfare_ppp) |>
+          imputation_id, weight, welfare_ppp) |>
   fsubset(distribution_type %in% c("micro", "imputed")) # 2 minutes
 
 # 2. Micro and Imputed Data: Level & Area Estimation  ----
@@ -17,9 +17,8 @@ dt_m <- cache_sac |>
 md_level <- dt_m |>
   fsubset(distribution_type %in% c("micro")) |>
   fselect(-c(distribution_type))|>
-  roworder(cache_id, imputation_id, pop_data_level, welfare_ppp) |>
-  collapse::join(means_out_sac |> 
-                   # GC Note: Added variables here and also joined by additional variables:
+  roworder(cache_id, pop_data_level, welfare_ppp) |>
+  collapse::join(mean_table |> 
                    fselect(cache_id, cpi_data_level, ppp_data_level,
                            gdp_data_level, pce_data_level,
                            pop_data_level, reporting_level, 
@@ -27,21 +26,23 @@ md_level <- dt_m |>
                  on=c("cache_id", "cpi_data_level", "ppp_data_level",
                       "gdp_data_level", "pce_data_level",
                       "pop_data_level", "reporting_level"), 
-                 # it is actually over-identified at this stage.
+                 # GC Note: it is actually over-identified at this stage. 
+                 # Maybe we can exlpore whether this is really needed?
                  validate = "m:1",
                  verbose = 0,
                  column = list(".joyn", c("x", "y", "x & y")))|>
   _[, as.list(wrp_md_dist_stats(welfare = welfare_ppp,
                                 weight  = weight,
                                 mean = funique(survey_mean_ppp))),
-    # Note: Here we remove area from the grouping too, and we add the other levels:
-    by = .(cache_id, imputation_id, cpi_data_level, ppp_data_level,
+    by = .(cache_id, cpi_data_level, ppp_data_level,
            gdp_data_level, pce_data_level,
            pop_data_level, reporting_level)]|>
   fungroup()|>
   frename(survey_median_ppp = median)|>
   fmutate(reporting_level = as.character(reporting_level),
-          pop_data_level = as.character(pop_data_level)) # 11:40 -> 11:43 3 minutes
+          pop_data_level = as.character(pop_data_level)) |>
+  fselect(-c(cpi_data_level, ppp_data_level,
+             gdp_data_level, pce_data_level)) # 3 minutes
 
 id_level <- dt_m |>
   fsubset(distribution_type %in% c("imputed")) |>
@@ -63,13 +64,16 @@ id_level <- dt_m |>
   fungroup()|>
   frename(survey_median_ppp = median)|>
   fmutate(reporting_level = as.character(reporting_level),
-          pop_data_level = as.character(pop_data_level)) # 11:43 -> 11:45 2 minutes
+          pop_data_level = as.character(pop_data_level))|>
+  fselect(-c(cpi_data_level, ppp_data_level,
+             gdp_data_level, pce_data_level)) # < 1 minute
+
 
 # 3. Micro and Imputed Data: National Estimation ----
 
 md_id_national <- dt_m |>
-  # this is equivalent to having pop_data_level > 1 and D2:
-  fsubset(reporting_level != 'national' & ppp_data_level != 'national') |> # here is the mistake!
+  # Gc Note: this is equivalent to having pop_data_level > 1 and D2 in cache_id:
+  fsubset(reporting_level != 'national' & ppp_data_level != 'national') |>
   fselect(-c(distribution_type))|>
   roworder(cache_id, imputation_id, welfare_ppp)|>
   _[, as.list(wrp_md_dist_stats(welfare = welfare_ppp, weight = weight)),
@@ -81,24 +85,16 @@ md_id_national <- dt_m |>
   fungroup()|>
   frename(survey_median_ppp = median)|>
   fmutate(reporting_level = as.character("national"),
-          cpi_data_level = as.character("national"),
-          ppp_data_level = as.character("national"),
-          pce_data_level = as.character("national"),
-          pop_data_level = as.character("national"),
-          gdp_data_level = as.character("national"))
+          pop_data_level = as.character("national")) # < 1 minute
 
 
 # Select variables, subset and join mean table
 dt_jn <- cache_sac |>
-  # GC NOte: same procedure as before, but we take welfare instead of welfare_ppp:
-  #fselect(cache_id, distribution_type, reporting_level, imputation_id,
-  #        area, weight, welfare)|>
   fselect(cache_id, distribution_type, imputation_id, cpi_data_level, ppp_data_level,
           gdp_data_level, pce_data_level,
           pop_data_level, reporting_level, weight, welfare) |>
   fsubset(distribution_type %in% c("group", "aggregate"))|>
-  collapse::join(means_out_sac |> 
-                   # GC Note: Added variables here and also joined by additional variables:
+  collapse::join(mean_table |> 
                    fselect(cache_id, cpi_data_level, ppp_data_level,
                            gdp_data_level, pce_data_level,
                            pop_data_level, reporting_level, 
@@ -106,10 +102,10 @@ dt_jn <- cache_sac |>
                  on=c("cache_id", "cpi_data_level", "ppp_data_level",
                       "gdp_data_level", "pce_data_level",
                       "pop_data_level", "reporting_level"), 
-                 # it is actually over-identified at this stage.
+                 # GC Note: it is actually over-identified at this stage as well.
                  validate = "m:1",
                  verbose = 0,
-                 column = list(".joyn", c("x", "y", "x & y")))
+                 column = list(".joyn", c("x", "y", "x & y"))) # immediate
 
 
 # MISSING WARNING MESSAGE
@@ -123,9 +119,8 @@ dt_jn <- dt_jn|>
 
 
 # this gives an error with wbpip, so we catch them as in TAR.
-gd_ag_area <- dt_jn |>
+gd_ag_level <- dt_jn |>
   fselect(-c(distribution_type, reporting_pop)) |>
-  #roworder(cache_id, reporting_level, area, welfare)|>
   roworder(cache_id, cpi_data_level, ppp_data_level,
            gdp_data_level, pce_data_level,
            pop_data_level, reporting_level, welfare) |>
@@ -137,7 +132,9 @@ gd_ag_area <- dt_jn |>
            pop_data_level, reporting_level)]|>
   frename(survey_median_ppp = median)|>
   fmutate(reporting_level = as.character(reporting_level),
-          pop_data_level = as.character(pop_data_level))
+          pop_data_level = as.character(pop_data_level))|>
+  fselect(-c(cpi_data_level, ppp_data_level,
+             gdp_data_level, pce_data_level)) # immediate
 
 
 setrename(gd_ag_area, gsub("deciles", "decile", names(gd_ag_area)))
@@ -147,11 +144,9 @@ setrename(gd_ag_area, gsub("deciles", "decile", names(gd_ag_area)))
 ag_syn <- dt_jn |>
   fsubset(distribution_type %in% c("aggregate")) |>
   fselect(-c(distribution_type))|>
-  # GC Note: We remove the area here:
   roworder(cache_id, cpi_data_level, ppp_data_level,
            gdp_data_level, pce_data_level,
            pop_data_level, reporting_level, welfare) |>
-  # GC NOte: And here too:
   fgroup_by(cache_id, cpi_data_level, ppp_data_level,
             gdp_data_level, pce_data_level,
             pop_data_level, reporting_level)|>
@@ -160,7 +155,7 @@ ag_syn <- dt_jn |>
     population = weight,
     mean = funique(survey_mean_ppp),
     pop = funique(reporting_pop))$welfare,
-    weight = funique(reporting_pop)/100000) 
+    weight = funique(reporting_pop)/100000) # immediate
 
 # Aggregate to national
 
@@ -175,119 +170,50 @@ ag_national <- ag_syn |>
   fungroup()|>
   frename(survey_median_ppp = median)|>
   fmutate(reporting_level = as.character("national"),
-          cpi_data_level = as.character("national"),
-          ppp_data_level = as.character("national"),
-          pce_data_level = as.character("national"),
-          pop_data_level = as.character("national"),
-          gdp_data_level = as.character("national"))
+          pop_data_level = as.character("national")) # immediate
+
+# Merge ----
+
+final <- rowbind(md_level, id_level, md_id_national, gd_ag_level, ag_national)
 
 
+# Clean-up ----
+
+dt_clean <- final |>
+  collapse::join(mean_table|>
+                   fselect("survey_id", "cache_id", "wb_region_code", "pcn_region_code",
+                           "country_code", "surveyid_year", "survey_year",
+                           "reporting_year", "survey_acronym", "welfare_type",
+                           "cpi", "ppp", "pop_data_level", "reporting_level"),
+                 on=c("cache_id", "reporting_level", "pop_data_level"), 
+                 validate = "1:1",
+                 how = "left",
+                 verbose = 0,
+                 overid = 2)|>
+  fmutate(survey_median_lcu = survey_median_ppp*ppp*cpi,
+          survey_id = toupper(survey_id))|>
+  fselect(-ppp, -cpi)|>
+  colorder(survey_id, cache_id, wb_region_code, pcn_region_code, country_code,
+           survey_acronym, surveyid_year, survey_year, reporting_year, welfare_type,
+           reporting_level, survey_median_lcu, survey_median_ppp, decile1:decile10,
+           mean, gini, polarization, mld, pop_data_level)
 
 
 # Comparison ----
 
-final <- rowbind(md_level |> fselect(-imputation_id), id_level, md_id_national, gd_ag_area, ag_national)
-
 # Create quick comparison with dist_out_tar
-compare_dist_sac <- final |> fselect(-c(cpi_data_level, ppp_data_level, gdp_data_level, pce_data_level))
+compare_dist_sac <- dt_clean
 setorder(compare_dist_sac, cache_id, reporting_level)
 compare_dist_sac <- as.data.table(lapply(compare_dist_sac, function(x) { attributes(x) <- NULL; return(x) }))
 
-compare_dist_tar <- dist_out_tar |> fselect(colnames(compare_dist_sac))
+compare_dist_tar <- dist_out_tar
 setorder(compare_dist_tar, cache_id, reporting_level)
 compare_dist_tar <- as.data.table(lapply(compare_dist_tar, function(x) { attributes(x) <- NULL; return(x) }))
 
 waldo::compare(compare_dist_tar, compare_dist_sac, tolerance = 1e-7) # All match
-waldo::compare(compare_dist_tar |> fselect(-c(polarization, mld)), 
-               compare_dist_sac |> fselect(-c(polarization, mld)), tolerance = 1e-7) # polarization and mld (only) do not match here
 
 
 
 
 
 
-
-
-
-
-# # NoD2 Comparison ----
-#   md_id_national_noD2 <-  md_id_national |>
-#     fmutate(source = grepl("D2", cache_id)) |>
-#     fsubset(source != TRUE)
-#   
-#   md_id_area_noD2 <- md_id_area |>
-#     fmutate(source = grepl("D2", cache_id)) |>
-#     fsubset(source != TRUE)
-#   
-#   gd_ag_area_noD2 <- gd_ag_area |>
-#     fmutate(source = grepl("D2", cache_id)) |>
-#     fsubset(source != TRUE)
-#   
-#   ag_national_noD2 <- ag_national |>
-#     fmutate(source = grepl("D2", cache_id)) |>
-#     fsubset(source != TRUE)
-#   
-#   dist_out_tar_noD2 <- dist_out_tar |>
-#     fmutate(source = grepl("D2", cache_id)) |>
-#     fsubset(source != TRUE)
-#     
-#   
-#   final_noD2 <- rowbind(md_id_area_noD2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization), 
-#                    md_id_national_noD2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization), 
-#                    gd_ag_area_noD2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization), 
-#                    ag_national_noD2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization))
-# 
-# 
-# # Create quick comparison with dist_out_ta
-# compare_dist_sac <- final_noD2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization)
-# setorder(compare_dist_sac, cache_id, reporting_level)
-# compare_dist_sac <- as.data.table(lapply(compare_dist_sac, function(x) { attributes(x) <- NULL; return(x) }))
-# 
-# compare_dist_tar <- dist_out_tar_noD2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization)
-# setorder(compare_dist_tar, cache_id, reporting_level)
-# compare_dist_tar <- as.data.table(lapply(compare_dist_tar, function(x) { attributes(x) <- NULL; return(x) }))
-# waldo::compare(compare_dist_tar, compare_dist_sac, tolerance = 1e-7)
-# # no differences
-# 
-# # Only D2 comparison ----
-# D2_cache_ids <- setdiff(final$cache_id, final_noD2$cache_id)
-# 
-# final_D2 <- rowbind(md_id_area |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(cache_id %in% D2_cache_ids), 
-#                     md_id_national |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(cache_id %in% D2_cache_ids), 
-#                     gd_ag_area |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(cache_id %in% D2_cache_ids), 
-#                     ag_national |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(cache_id %in% D2_cache_ids))
-# 
-# dist_out_tar_D2 <- dist_out_tar |> fsubset(cache_id %in% D2_cache_ids)
-# 
-# compare_dist_sac <- final_D2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization)
-# setorder(compare_dist_sac, cache_id, reporting_level)
-# compare_dist_sac <- as.data.table(lapply(compare_dist_sac, function(x) { attributes(x) <- NULL; return(x) }))
-# 
-# compare_dist_tar <- dist_out_tar_D2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization)
-# setorder(compare_dist_tar, cache_id, reporting_level)
-# compare_dist_tar <- as.data.table(lapply(compare_dist_tar, function(x) { attributes(x) <- NULL; return(x) }))
-# waldo::compare(compare_dist_tar, compare_dist_sac, tolerance = 1e-7, max_diffs = Inf)
-# 
-# # Only NOT micro and D2 ----
-# micro_D2_cache_ids <- cache_sac |> 
-#   fselect(cache_id, distribution_type) |> 
-#   fsubset(distribution_type == "micro") |>
-#   fmutate(source = grepl("D2", cache_id)) |>
-#   fsubset(source)
-#   
-# final_no_micro_D2 <- rowbind(md_id_area |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(!cache_id %in% micro_D2_cache_ids$cache_id), 
-#                              md_id_national |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(!cache_id %in% micro_D2_cache_ids$cache_id), 
-#                              gd_ag_area |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(!cache_id %in% micro_D2_cache_ids$cache_id), 
-#                              ag_national |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization) |> fsubset(!cache_id %in% micro_D2_cache_ids$cache_id))
-# 
-# dist_out_tar_no_micro_D2 <- dist_out_tar |> fsubset(!cache_id %in% micro_D2_cache_ids$cache_id)
-# 
-# 
-# compare_dist_sac <- final_no_micro_D2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization)
-# setorder(compare_dist_sac, cache_id, reporting_level)
-# compare_dist_sac <- as.data.table(lapply(compare_dist_sac, function(x) { attributes(x) <- NULL; return(x) }))
-# 
-# compare_dist_tar <- dist_out_tar_no_micro_D2 |> fselect(cache_id, reporting_level, decile1, decile5, decile10, mean, polarization)
-# setorder(compare_dist_tar, cache_id, reporting_level)
-# compare_dist_tar <- as.data.table(lapply(compare_dist_tar, function(x) { attributes(x) <- NULL; return(x) }))
-# waldo::compare(compare_dist_tar, compare_dist_sac, tolerance , max_diffs = Inf)
